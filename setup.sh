cbcc47aa3871db4d1ddba1bd5c9bf22086c52cf3
Chris Spinden <cjs77@njit.edu>
2025-06-16 02:02:26 -0400

    Adding Setup Script for VM Baseline

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt upgrade
+wget https://github.com/MattToegel/IT490/archive/refs/heads/master.zip
+unzip master.zip
+cd IT490-master/
+sudo apt install zip
+sudo apt install php
+sudo apt install nano
+sudo apt install composer
+sudo apt install rabbitmq-server
+sudo apt install net-tools
+sudo apt install openssh-server
+composer update
