5ff12e572d9596738b682cd133e23cf142a857ea
Chris Spinden <cjs77@njit.edu>
2025-06-16 23:15:50 -0400

    Final Basic Setup Script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
+#!/usr/bin/env bash
+
 sudo apt update
 sudo apt upgrade
-wget https://github.com/MattToegel/IT490/archive/refs/heads/master.zip
-unzip master.zip
-cd IT490-master/
 sudo apt install zip
 sudo apt install php
 sudo apt install nano
 sudo apt install composer
 sudo apt install rabbitmq-server
 sudo apt install net-tools
+
+sudo systemctl enable rabbitmq-server
+sudo systemctl start rabbitmq-server
+
 sudo apt install openssh-server
+sudo systemctl enable openssh-server
+sudo systemctl start openssh-server
+
+wget https://github.com/MattToegel/IT490/archive/refs/heads/master.zip
+unzip master.zip
+cd IT490-master/
 composer update
